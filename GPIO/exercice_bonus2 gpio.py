91fb91d56b85730fe92999e8c5b0642346c58d15
hepl-duspeaux <antoine.duspeaux@student.hepl.be>
2024-09-28 10:58:21 +0200

    Add files via upload

---
+++ GPIO/exercice_bonus2 gpio.py
@@ -0,0 +1,44 @@
+import machine
+import utime
+from machine import Pin
+LED = machine.Pin(16,machine.Pin.OUT)
+BUTTON = machine.Pin(18,machine.Pin.IN)
+value = 0
+
+
+def fct_interruption(pin):
+
+    global value
+    value+=1
+    
+    print(value)
+        
+BUTTON.irq(trigger = Pin.IRQ_RISING, handler = fct_interruption)
+#pin_button.irq(trigger=Pin.IRQ_FALLING,handler=button_isr)
+while True:
+
+    
+    if value == 1:
+        LED.value(1)
+        utime.sleep(2)
+        LED.value(0)
+        utime.sleep(2)
+        
+    elif value == 3:
+        LED.value(1)
+        utime.sleep(1)
+        LED.value(0)
+        utime.sleep(1)
+        
+    elif value == 5:
+        j=1
+        while j<=500:
+            LED.value(1)
+            utime.sleep_ms(j)
+            LED.value(0)
+            utime.sleep_ms(j)
+            j+=50
+        
+    elif value == 6:
+        value =0
+    
