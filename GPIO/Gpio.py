3e779b541add2bdf83f984b4058c3a291496376a
hepl-duspeaux <antoine.duspeaux@student.hepl.be>
2024-09-28 10:40:19 +0200

    Update Gpio.py

--- GPIO/Gpio.py
+++ GPIO/Gpio.py
@@ -1,17 +1,35 @@
 import machine
 import utime
-
+from machine import Pin
 LED = machine.Pin(16,machine.Pin.OUT)
 BUTTON = machine.Pin(18,machine.Pin.IN)
-val = 0
+value = 0
 
 
+def fct_interruption(pin):
+
+    global value
+    value+=1
+    
+    print(value)
+        
+BUTTON.irq(trigger = Pin.IRQ_RISING, handler = fct_interruption)
+#pin_button.irq(trigger=Pin.IRQ_FALLING,handler=button_isr)
 while True:
 
-    if BUTTON.value() == 1:
-        val = val+1
-        utime.sleep(0.25)
-    elif val == 2:
-        val =0
-        utime.sleep(0.25)
-    LED.value(val)
+    
+    if value == 1:
+        LED.value(1)
+        utime.sleep(2)
+        LED.value(0)
+        utime.sleep(2)
+        
+    elif value == 2:
+        LED.value(1)
+        utime.sleep(1)
+        LED.value(0)
+        utime.sleep(1)
+        
+    
+    elif value == 3:
+        value =0
